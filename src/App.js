71a731fcbe6fa6ab8a6fd035805b7a28f7f859fa
Ajit133 <dasajit697@gmail.com>
2022-06-16 03:05:29 +0600

    Add  Toggle and some function

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Book from './components/book';
 class App extends Component{
   state = {
     books:[
-      {bookName:'1984',writer:"George Orewell"},
-      {bookName:"The Da Vinci Code",writer:"Dan Brown"},
-      {bookName:"The Alchemist",writer:"Paulo Coelho"}
-    ]
+      {id:1, bookName:'1984',writer:"George Orewell"},
+      {id:2, bookName:"The Da Vinci Code",writer:"Dan Brown"},
+      {id:3, bookName:"The Alchemist",writer:"Paulo Coelho"}
+    ],
+    showBooks : true
   }
 
 deleteBookState = (index) =>{
@@ -17,26 +18,46 @@ deleteBookState = (index) =>{
   this.setState({
     books:books
   });
+};
+changeWithinput = (event,index) =>{
+ const book = {
+    ...this.state.books[index]
+ }
+ book.bookName = event.target.value;
+ const books = [...this.state.books];
+ books[index]=book;
+ this.setState({books:books});
 
 };
+ToggleBooks = () =>{
+  this.setState({showBooks : !this.state.showBooks})
+
+}
 
   render(){
-    const books = this.state.books.map( (book,index) =>{
+    let books = null 
+    if (this.state.showBooks){
+    books = this.state.books.map( (book,index) =>{
       return(
         <div>
-          
+          <h2>Book List</h2>  
         <Book 
         bookName={book.bookName} 
         writer = {book.writer} 
         delete = {()=>(this.deleteBookState(index))}
+        key = {book.id}
+        input = {(event)=>this.changeWithinput(event,index)}
+        
         />
         </div>
+    
       )
-    })
+    })}
     return(
       <div className='App'>
         <h1>This is Book List</h1>
-      {books}
+        <button onClick={this.ToggleBooks}>Toggle Books</button>
+       {books}
       </div>
     )
   }
--- src/components/book.js
+++ src/components/book.js
@@ -5,6 +5,7 @@ const Book = (props) =>{
     <div>
       <h2 onClick={props.delete} > Book Name is {props.bookName}</h2>
       <h2>Writer Name is {props.writer}</h2>
+      <input  onChange={props.input} type='text' value={props.bookName} />
     </div>
    )
 }
